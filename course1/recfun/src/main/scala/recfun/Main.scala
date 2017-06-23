711d8619700f18e7142ea99d7a4c9916f58ed3b1
ktz <knightpop@gmail.com>
2017-06-24 03:38:47 +0900

    first commit

--- course1/recfun/src/main/scala/recfun/Main.scala
+++ course1/recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -13,12 +15,27 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      def isFirstElement(c: Int, r: Int): Boolean = c == 0 || c == r
+      if(isFirstElement(c, r)) 1
+      else pascal(c - 1, r - 1) + pascal(c, r -1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+
+      @tailrec
+      def loop(nOfLeftParentheses: Int, nOfRightParentheses: Int, chars: List[Char]): Boolean =
+        if(chars.isEmpty) nOfLeftParentheses == nOfRightParentheses
+        else if(nOfLeftParentheses < nOfRightParentheses) false
+        else if(chars.head == '(') loop(nOfLeftParentheses + 1, nOfRightParentheses, chars.tail)
+        else if(chars.head == ')') loop(nOfLeftParentheses, nOfRightParentheses + 1, chars.tail)
+        else loop(nOfLeftParentheses, nOfRightParentheses, chars.tail)
+
+      loop(0, 0, chars)
+    }
   
   /**
    * Exercise 3
