7486378867161c83d5256886341ebdb54beb1c5d
ktz <knightpop@gmail.com>
2017-08-19 19:08:01 +0900

    first Commit

--- course2/streams/src/main/scala/streams/Solver.scala
+++ course2/streams/src/main/scala/streams/Solver.scala
@@ -65,7 +65,7 @@ trait Solver extends GameDef {
    */
   def from(initial: Stream[(Block, List[Move])],
            explored: Set[Block]): Stream[(Block, List[Move])] = initial match {
-    case Stream.Empty => Stream.empty
+    case Stream.Empty=> Stream.empty
     case (block, moveList) #:: tail =>
       val newExplored = explored + block
       val newNeighbors = newNeighborsOnly(neighborsWithHistory(block, moveList), newExplored)
@@ -76,13 +76,17 @@ trait Solver extends GameDef {
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(
+    (startBlock -> List.empty) #:: Stream.empty,
+    Set.empty
+  )
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] =
+    pathsFromStart.filter(moveCase => moveCase._1.isStanding && moveCase._1.b1 == goal)
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -92,5 +96,5 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal.minBy(_._2.length)._2
 }
